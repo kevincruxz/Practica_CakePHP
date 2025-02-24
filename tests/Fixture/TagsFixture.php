2a26249af15ab069a9cbff88e0545861757dca10
kevin <kevin_cruzz@outlook.es>
2025-02-24 17:51:33 -0600

    primer commit, se agrega todo el proyecto

---
+++ tests/Fixture/TagsFixture.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * TagsFixture
+ */
+class TagsFixture extends TestFixture
+{
+    /**
+     * Init method
+     *
+     * @return void
+     */
+    public function init(): void
+    {
+        $this->records = [
+            [
+                'id' => 1,
+                'title' => 'Lorem ipsum dolor sit amet',
+                'created' => '2025-02-18 23:34:27',
+                'modified' => '2025-02-18 23:34:27',
+            ],
+        ];
+        parent::init();
+    }
+}
